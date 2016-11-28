5579e9f52d57ad6a332e1d42c83e79dfb4f5c7d8
Chickenman222 <Miros.kohutos@gmail.com>
2016-11-28 14:01:39 +0100

    New_Spi_function
    Added new spi_write_read function. Not tested

--- src/functions.c
+++ src/functions.c
@@ -126,5 +126,14 @@ void init_SPI1(void){
 	SPI_Cmd(SPI1, ENABLE); // enable SPI1
 }
 
+uint8_t SPI1_receive_send(uint8_t data){
+
+	SPI1->DR = data;                         // write data to be transmitted to the SPI data register
+	while( !(SPI1->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+	while( !(SPI1->SR & SPI_I2S_FLAG_RXNE) );// wait until receive complete
+	while( SPI1->SR & SPI_I2S_FLAG_BSY );    // wait until SPI is not busy anymore
+	return SPI1->DR;                         // return received data from SPI data register
+}
+
 
 
